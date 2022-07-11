1682c5d82a5a0e806dc5b36a4213ccb7d6bfa74f
Gyuho Lee <gyuho.lee@avalabs.org>
2022-07-11 19:58:59 +0800

    *: initial commit, 0.0.1
    Signed-off-by: Gyuho Lee <gyuho.lee@avalabs.org>

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# prometheus-manager
-Prometheus manager
+
+Rust random manager
+
+![Crates.io](https://img.shields.io/crates/v/prometheus-manager?logo=rust&style=for-the-badge)
+
+https://crates.io/crates/prometheus-manager
---
+++ scripts/tests.unit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -xue
+
+if ! [[ "$0" =~ scripts/tests.unit.sh ]]; then
+  echo "must be run from repository root"
+  exit 255
+fi
+
+RUST_LOG=debug cargo test --workspace --all-features -- --show-output
+
+echo "ALL SUCCESS!"
